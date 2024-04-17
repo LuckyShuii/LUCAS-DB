4f5c72bbee8bb47a8f25736137c10423b2d64558
LuckyShuii <lucasnimes30000@gmail.com>
2024-04-17 21:07:37 +0200

    Added primevue to config

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,10 @@
 import './assets/styles.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 const app = createApp(App)
+
+app.use(PrimeVue);
 
 app.mount('#app')
